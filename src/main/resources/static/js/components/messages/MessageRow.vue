3c36b9a548af263f2eb287f12c66c69e2e764bb3
tmvf <tmvf@yandex.ru>
2018-10-23 10:18:37 +0300

    06

---
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b>{{message.id}}. </b>
+        {{message.text}}
+        <span style="position: absolute; right: 0">
+       <input type='button' value='Edit' @click='edit'>
+ <input type='button' value='X' @click='del'>
+ </span>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['message', 'editMessage', 'deleteMessage','messages'],
+        methods: {
+            edit() {
+                this.editMessage(this.message)
+            },
+            del() {
+                this.deleteMessage(this.message)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
